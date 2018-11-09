9c7016d84a1b7b2626f6e9118b14a2bf0ec65665
JeanAnx <jean.annaix@gmail.com>
2018-11-09 16:21:59 +0100

    Ajout d'une boucle 'for of'

---
+++ atelierJS2.js
@@ -0,0 +1,54 @@
+var currentImage = document.getElementById('currentImage');
+
+function getTheBeatles() {
+    var lineUp = document.querySelectorAll('.beatles');
+    console.log(lineUp)
+    return lineUp;
+};
+
+var theBeatles = getTheBeatles();
+console.log(theBeatles);
+
+for (let i of theBeatles) {
+
+    i.addEventListener('mouseover',function(e) {
+
+        console.log("hover");
+        this.style.height = "250px";
+        this.style.opacity = "1";
+
+    });
+
+    i.addEventListener('mouseout',function(e) {
+
+        console.log("plus hover");
+        this.style.height = "200px";
+        this.style.opacity = "0.5";
+
+    });
+
+    i.addEventListener('click',function() {
+
+        var thisAttribute = this.getAttribute('src');
+        currentImage.setAttribute('src',thisAttribute);   
+    })
+
+    
+
+
+};
+
+
+// var john = document.getElementById('john');
+// console.log(john);
+// function findTheBeatle(beatle){
+//     var thisBeatle = beatle.getAttribute('id');
+//     console.log(thisBeatle);
+//     return thisBeatle;
+// }
+
+
+// document.querySelectorAll('.beatles').addEventListener('click' , function() {
+//     findTheBeatle(this);
+// });
+
