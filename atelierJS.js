3c1b093512439588f9c822be71e4db49a3231f18
JeanAnx <jean.annaix@gmail.com>
2018-11-09 14:05:02 +0100

    Fin exercice Formulaire

--- atelierJS.js
+++ atelierJS.js
@@ -1,97 +1,85 @@
+    /* MES VARIABLES */
+
 var prenom = document.getElementById("prenom");
 var nom = document.getElementById("nom");
 var email = document.getElementById("email");
+var leBouton = document.getElementById("leBouton");
 var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    /* MES FONCTIONS */
 
+function checkInputText(input) {
+    console.log("Value : " + input.value )
+    if (!isNaN(input.value) || (input.value == "")) {
+        input.style.backgroundColor = "red";
+        return false;       
+        } else {
+            input.style.backgroundColor = "green";
+            return true;
+        }
+};
 
-prenom.addEventListener('blur',function() {
-    console.log('Perte focus');
-    console.log("Value : " + this.value )
+function checkInputNumber(input) {
+    var theValue = input.value
+    console.log("Value : " + theValue )
+    if (isNaN(theValue) || (theValue == "") || (input.value < 18 ) || (input.value > 100) ) {       
+        input.style.backgroundColor = "red";
+        return false;
+        } else {
+            input.style.backgroundColor = "green";
+            return true;
+        }
+};
 
-    if (!isNaN(this.value) || (this.value == "")) {
+function checkEmail(input) {
+    var theValue = input.value;
+    if (regex.test(theValue) == false || (theValue == "")) {
+        input.style.backgroundColor = "red"; 
+        return false;
+        } else {                
+            input.style.backgroundColor = "green";
+            return true;            
+        }
+};
 
-        this.style.backgroundColor = "red";
-        
+function checkForm() {
+    if ( checkInputText(prenom) == false || checkInputText(nom) == false || checkInputNumber(age) == false || checkEmail(email) == false ) {
+        console.log(checkInputText(prenom));
+        return false;
     } else {
-
-        this.style.backgroundColor = "green";
+        return true;
     }
+}
 
+function getSafe(string) {
+    var reject = /<script>/g;
+    string.replace( reject , "&gt");
+    return string;
+}
+
+var text = "Lorem <script>";
+console.log(getSafe(text));
+
+    /* MES ÉVÈNEMENTS */
+
+
+prenom.addEventListener('blur', function() {
+    checkInputText(this);
 });
 
 nom.addEventListener('blur',function() {
-    console.log('Perte focus');
-    console.log("Value : " + this.value )
-    if (!isNaN(parseInt(this.value)) || (this.value == "")) {
-        this.style.backgroundColor = "red";
-        
-    } else {
-        this.style.backgroundColor = "green";
-    }
-
+    checkInputText(this);
 });
 
+
 age.addEventListener('blur',function() {
-    console.log('Perte focus');
-    console.log("Value : " + this.value )
-    if (isNaN(this.value) || (this.value == "")) {
-        
-        this.style.backgroundColor = "red";
-        
-    } else {
-
-        this.style.backgroundColor = "green";
-
-    }
-
+    checkInputNumber(this);
 });
 
 email.addEventListener('blur',function() {
-
-    console.log('Chek Mail');
-
-    if (regex.test(this.value) == false || (this.value == "")) {
-        
-        this.style.backgroundColor = "red";
-  
-    } else {
-        
-        this.style.backgroundColor = "green";
-        
-    }
-})
+    checkEmail(this);
+});
 
 
-
-/***************************************************************/
-
-
-
-//leBouton = document.getElementById('leBouton');
-// console.log(leBouton);
-// var titre = document.getElementById('Titre');
-// console.log(titre);
-
-// leBouton.addEventListener('click', function() {
-//     var a = prompt('Veuillez entrer un nombre');
-
-//     if ((isNaN(a))) {
-
-//         a = "A n'est pas un nombre espèce de Boloss";
-    
-//     }
-
-//     titre.innerHTML = a;
-
-// });
-
-// function insertionNombre () {
-//     var a = prompt('Veuillez entrer un nombre');
-//     var titre = document.getElementById('Titre');
-//     titre.innerHTML = a;
-// }
-
-/***************************************************************/
 
 
